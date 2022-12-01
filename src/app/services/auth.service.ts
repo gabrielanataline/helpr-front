e12c6f5c2bf0dbdd2e756ea2c664e6156fc4e039
gabrielanataline <gabiesnataline@hotmail.com>
2022-12-01 14:59:08 -0300

    Criação do AuthService, Credenciais, Token e API_CONFIG

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { Token } from './../models/token';
+import { Credenciais } from './../models/credenciais';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, EMPTY } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { API_CONFIG } from '../config/api.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  public authenticate(credenciais: Credenciais): Observable<Token> {
+    
+    return this.http.post<Token>(`${API_CONFIG.baseUrl}/auth/login`, credenciais).pipe(
+      tap(token => {
+        localStorage.setItem("token", token.accessToken);
+      }),
+      catchError(error => {
+        alert("Erro ao autenticar!")
+        console.error(error);
+        return EMPTY;
+
+      })
+    );
+  }
+}
