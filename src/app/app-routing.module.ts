5cf66dc51b6d38305cf3d61f7e195f561b7d46e7
gabrielanataline <gabiesnataline@hotmail.com>
2022-12-02 09:14:48 -0300

    authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [{ path: 'login', 
-loadChildren: () => import('./views/login/login.module').then(m => m.LoginModule) }];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import("./views/login/login.module").then(m => m.LoginModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule),
+    canActivate: [ AuthGuard ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
